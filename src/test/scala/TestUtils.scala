54862691624695dfbb5c895c528fb982f8b2cd0a
chick <chick@qrhino.com>
2019-11-19 09:33:32 -0800

    Reformatted table for chisel3 style

--- src/test/scala/TestUtils.scala
+++ src/test/scala/TestUtils.scala
@@ -224,7 +224,7 @@ abstract class IntegrationTests[T <: BasicTester : ClassTag](
     val subresults = subtests map { test =>
       val stream = getClass.getResourceAsStream(s"/$test.hex")
       val loadmem = io.Source.fromInputStream(stream).getLines
-      Future(test -> (TesterDriver execute (() => tester(loadmem, testType.maxcycles))))
+      Future(test -> (TesterDriver.execute(() => tester(loadmem, testType.maxcycles), nameHint = Some(test))))
     }
     Await.result(Future.sequence(subresults), Duration.Inf)
   }
